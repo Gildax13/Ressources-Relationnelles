83cc191c279827d8f462ee93373b733dd2fd8a45
Gildax13 <60438399+Gildax13@users.noreply.github.com>
2023-05-07 19:22:36 +0200

    Modif formulaire

--- resources/views/confirmressource.blade.php
+++ resources/views/confirmressource.blade.php
@@ -5,21 +5,21 @@
         </h2>
     </x-slot>
     <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6">
-    <br>
-    <div class="container">
-        <div class="row card text-white bg-dark">
-            <h4 class="card-header">Merci pour l'ajout de votre ressource</h4>
-            <div class="card-body">
-                <p class="card-text">Votre ressource a bien été ajouter dans le système</p>
+                    <br>
+                    <div class="container">
+                        <div class="row card text-white bg-dark">
+                            <h4 class="card-header">Merci pour l'ajout de votre ressource</h4>
+                            <div class="card-body">
+                                <p class="card-text">Votre ressource a bien été ajouter dans le système</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </form>
+        </form>
     </div>
 </x-app-layout>
